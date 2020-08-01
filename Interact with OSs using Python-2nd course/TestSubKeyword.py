5d6089c0307cb42ac5b54c41ed1f66e0636c17e0
mastrmind-dev <asapthaka@gmail.com>
2020-08-01 13:13:16 +0530

    Test keyword sub with some advanced exercise

--- Interact with OSs using Python-2nd course/TestSubKeyword.py
+++ Interact with OSs using Python-2nd course/TestSubKeyword.py
@@ -2,7 +2,7 @@ import re
 
 s = '456-456-456'
 
-result = re.sub('([0-9]{3}-[0-9])', '+1-\\1', s)
+result = re.sub('[0-9]{3}-([0-9])', '+1-\\1', s)
 print(result)
 result = re.sub('([0-9]{3}-[0-9])', r'+1-\1', s)
 print(result)
